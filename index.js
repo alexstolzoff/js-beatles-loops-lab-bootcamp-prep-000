c4f4160178f7c52ebb6a67eeb53103af09e870ad
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-26 06:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const musiciansArray = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instrumentsArray = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musiciansArray, instrumentArray){
-  const musiciansArray = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  const instrumentsArray = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var stringArray = [];
   for(let i=0;i<musiciansArray.length();i++){
     stringArray.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
